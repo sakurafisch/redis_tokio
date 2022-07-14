49a9a546275b789a32ea231ed6ab2ab31215f1e6
sakurafisch <sakurafisch@gmail.com>
2022-07-14 22:51:45 +0800

    feat: 并发

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-// use tokio::net::{TcpListener, TcpStream};
-// use mini_redis::{Connection, Frame};
-
-// #[tokio::main]
-// async fn main() {
-//     // Bind the listener to the address
-//     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
-//     loop {
-//         // The second item contains the IP and port of the new connection.
-//         let (socket, _) = listener.accept().await.unwrap();
-//         process(socket).await;
-//     }
-// }
-
-// async fn process(socket: TcpStream) {
-//     let mut connection = Connection::new(socket);
-//     if let Some(frame) = connection.read_frame().await.unwrap() {
-//         println!("GOT: {:?}", frame);
-
-//         // Respond with an error
-//         let response = Frame::Error("unimplemented".to_string());
-//         connection.write_frame(&response).await.unwrap();
-//     }
-// }
-
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 
@@ -35,7 +9,9 @@ async fn main() {
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
@@ -52,3 +28,5 @@ async fn process(socket: TcpStream) {
         connection.write_frame(&response).await.unwrap();
     }
 }
+
+
